08274a3d42dadf342fcc1a7d42d5402b290bde79
lukoki Makuntima Daniel <lukokimakuntimadaniel@gmail.com>
2023-05-07 16:43:50 +0100

    Adicionar arquivos de projeto.

---
+++ Problema_dos_sucessores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_dos_sucessores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            int n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                numeros.Add(n);
+                n = int.Parse(Console.ReadLine());
+            }
+
+            foreach(var v in numeros)
+            {
+                Console.WriteLine(v+1);
+            }
+        }
+    }
+}
